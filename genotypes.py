6031e95a6bb3c1b091b3b21b5fc593cff3b4bad9
johsnows <sdut_snow@163.com>
2021-01-20 19:52:38 +0800

    update

--- genotypes.py
+++ genotypes.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 import torch
 import torch.nn as nn
-from .models import ops
+from models import ops
 
 
 Genotype = namedtuple('Genotype', 'normal normal_concat reduce reduce_concat')
