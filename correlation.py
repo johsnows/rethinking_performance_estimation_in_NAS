2684c13d32329f142634096c296e16ef50afcd79
johsnows <sdut_snow@163.com>
2021-01-21 16:46:17 +0800

    update

--- correlation.py
+++ correlation.py
@@ -7,7 +7,7 @@ from scipy.stats import kendalltau
 
 def draw_linears_y(x, y, name):
     n = len(y)
-    fig, axes = plt.subplots(nrows=2, ncols=n//2+1, figsize=(20, 10))
+    fig, axes = plt.subplots(nrows=2, ncols=n//2+1, figsize=(30, 10))
     for i in range(n):
         if i//5==0:
             axes[0][i].plot(x, y[i])
