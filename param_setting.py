514d93462f95205c33ebd2420a0faa68de136897
johsnows <sdut_snow@163.com>
2021-01-22 21:18:11 +0800

    update

--- param_setting.py
+++ param_setting.py
@@ -1,19 +1,19 @@
 param_BPE1 = {
     'epochs': 10, 
-    'batchsize': 128,
+    'batch_size': 128,
     'lr': 0.03,
     'layers': 6,
-    'channel': 8,
-    'imagesize': 16,
+    'init_channels': 8,
+    'image_size': 16,
     'fp': 32,
-    'cutout': 0 }
+    'cutout_length': 0 }
 
 param_BPE2 = {
     'epochs': 30, 
-    'batchsize': 128,
+    'batch_size': 128,
     'lr': 0.03,
     'layers': 16,
-    'channel': 16,
-    'imagesize': 16,
+    'init_channels': 16,
+    'image_size': 16,
     'fp': 32, 
-    'cutout': 0 }
+    'cutout_length': 0 }
