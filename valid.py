8723d2a3cde596edb61b3be1045a970838f2db90
johsnows <sdut_snow@163.com>
2021-01-25 15:08:49 +0800

    update

--- valid.py
+++ valid.py
@@ -1,5 +1,5 @@
 import torch
-from scipy.stats import kendalltau
+from scipy.stats import kendalltau, spearmanr
 import torch.nn as nn
 import numpy as np
 from models.augment_cnn import AugmentCNN
@@ -76,15 +76,17 @@ def main():
     # change size of input image
     input_size = config.image_size
 
-    ckpt = "experiment/darts_10/{}/checkpoint10.pth.tar"
+    ckpt = "experiment/bpe_darts_10/{}/checkpoint{}.pth.tar"
+    epochs = 10
     file_ = open(config.file)
     lines = file_.readlines()
     kends = []
-    for epoch in range(50):
+    spearms = []
+    for epoch in range(epochs):
         top1s = []
         for i, line in enumerate(lines):
-            if i>23:
-                break
+            # if i>23:
+            #     break
         # for i in range(24):
             model = AugmentCNN(input_size, input_channels, config.init_channels, n_classes, config.layers,
                                    use_aux, geno.from_str(line))
@@ -92,7 +94,7 @@ def main():
             mb_params = utils.param_size(model)
             print("Model size = {:.3f} MB".format(mb_params))
             model = nn.DataParallel(model, device_ids=config.gpus).to(device)
-            model =torch.load(ckpt.format(i))
+            model =torch.load(ckpt.format(i, epoch))
             if isinstance(model, torch.nn.DataParallel):
                 model = model.module
             valid_loader = torch.utils.data.DataLoader(valid_data,
@@ -103,14 +105,25 @@ def main():
             top1 = validate(valid_loader, model, criterion, 10)
             top1s.append(top1)
         top1s = np.array(top1s)
-        np.save("binar_epoch{}_acc.npy".format(epoch), top1s)
-        gt = [ 96.82, 97.28, 96.98, 97.19, 96.87, 96.76, 97.09, 97.24, 97.26, 96.92, 96.82, 97.09, 96.62, 97.02, 97.18, 97.15, 97.32, 97.05, 97.02, 97.16, 97.34, 97.00, 97.19, 96.51, ]
+        np.save("res/binary_bpe1_epoch{}_acc.npy".format(epoch), top1s)
+        # gt = [ 96.82, 97.28, 96.98, 97.19, 96.87, 96.76, 97.09, 97.24, 97.26, 96.92, 96.82, 97.09, 96.62, 97.02, 97.18, 97.15, 97.32, 97.05, 97.02, 97.16, 97.34, 97.00, 97.19, 96.51, ]
+        gt = [96.82, 97.28, 96.98, 97.19, 96.87, 96.76, 97.09, 97.24, 97.26, 96.92, 96.82, 97.09, 96.62, 97.02, 97.18,
+              97.15, 97.32, 97.05, 97.02, 97.16, 97.34, 97.00, 97.19, 96.51, 96.85, 96.56, 96.89, 97.01, 97.15, 97.10,
+              96.87, 96.59, 96.93, 97.24, 97.19, 97.14, 96.55, 97.04, 97.08, 96.95, 97.40, 96.97, 97.16, 96.90, 96.78,
+              96.94, 97.12, 97.13, 97.05, 97.04, 96.55, 96.92, 96.80, 97.08, 97.28, 96.94, 97.08, 97.11, 97.35, 97.15,
+              97.07, 96.93, 96.40, 97.02, 96.85, 96.79, 96.18, 96.75, 97.02, 97.45, 97.08, 97.41, 96.96, 96.52, 96.87,
+              96.47, 97.20, 96.31, 96.99, 97.13, 97.16, 97.08, 97.07, 96.62, 97.18, 97.20, 97.11, 96.81, 96.90, 97.21,
+              96.94, 96.96, 96.81, 96.93, 97.36, 97.01, 97.12, 97.05, 96.85, 97.38, ]
         kend , _=  kendalltau(gt, top1s)
+        spearm , _=  spearmanr(gt, top1s)
         print('kend', kend)
+        print('spearm', spearm)
         kends.append(kend)
+        spearms.append(spearm)
     from correlation import draw_linear
-    epochs = [i for i in range(50)]
-    draw_linear(epochs, kends, 'binary_kend.pdf')
+    epochs = [i for i in range(epochs)]
+    draw_linear(epochs, kends, 'bpe1_binary_kend.pdf')
+    draw_linear(epochs, spearms, 'bpe1_binary_spearm.pdf')
 
 
 
