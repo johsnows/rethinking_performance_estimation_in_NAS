825ac34aed6e64f9acc90881da7bf21921d8b350
johsnows <sdut_snow@163.com>
2021-01-27 20:59:37 +0800

    update

--- run_augment.sh
+++ run_augment.sh
@@ -1,6 +1,6 @@
 for((i=0; i<100; i=i+1));
 do
-        run_cmd="python binary_augment.py --name test_bpe0_six --file random_darts_architecture.txt --bpe 1 gpus 0,1,2,3,4,5,6,7"
+        run_cmd="python3 binary_augment.py --name test_bpe0_six --file random_darts_architecture.txt --bpe 1 gpus 0,1,2,3,4,5,6,7"
         echo ${run_cmd}
         echo ${i}
         $run_cmd
