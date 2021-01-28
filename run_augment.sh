335d9dc0153be323c6ca763c16a86e3d0a58e043
johsnows <sdut_snow@163.com>
2021-01-28 21:07:42 +0800

    update

--- run_augment.sh
+++ run_augment.sh
@@ -1,6 +1,6 @@
 for((i=0; i<100; i=i+1));
 do
-        run_cmd="nohup python3 -m torch.distributed.launch --nproc_per_node=8 --master_port 29501 binary_augment.py --name test_bpe0_six --file random_darts_architecture.txt --bpe 1 --gpus all >bpe0_six&"
+        run_cmd="nohup python3 -m torch.distributed.launch --nproc_per_node=8 --master_port 29501 binary_augment.py --name test_bpe0_six --file random_darts_architecture.txt --bpe 1 --gpus all"
         echo ${run_cmd}
         echo ${i}
         $run_cmd
